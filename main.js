dc9d8e1fa2fda98e9c204dfdea02456d71b81156
Azia Bay-Asen <aziabayasen@gmail.com>
2024-03-24 19:34:10 -0500

    feat: animations

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-// On menu icon tapped
-document.querySelector(".menu-icon").addEventListener("click", () => {
-  var menu = document.querySelector(".menu");
-  menu.style.display = menu.style.display == "flex" ? "none" : "flex";
+new WOW({animateClass: 'animate__animated'}).init();
+
+const contactButton = document.querySelector('.navigation-links a:last-of-type');
+contactButton.addEventListener('click', pulseSocials);
+
+function pulseSocials() {
+  var socials = document.querySelectorAll('#contact .box');
+  socials.forEach(social => {
+    if (!social.classList.contains('animate__heartBeat'))
+      animateCSS(social, 'heartBeat');
+  });
+}
+
+const companies = document.querySelectorAll('.companies-section .box');
+companies.forEach(company => {
+  company.addEventListener('mouseover', pulseCompany);
 });
 
-// On outside navigation bar tapped
-document.addEventListener('click', (e) => {
-  if(document.querySelector("nav").contains(e.target)) return;
-  document.querySelector(".menu").style.display = "none";
-});
+function pulseCompany(event) {
+  node = event.target;
+  if (!node.classList.contains('animate__pulse'))
+    animateCSS(node, 'pulse');
+}
+
+const animateCSS = (node, animation, prefix = 'animate__') =>
+  new Promise((resolve, _reject) => {
+    const animationName = `${prefix}${animation}`;
+    node.classList.add(`${prefix}animated`, animationName);
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve('Animation ended');
+    }
+    node.addEventListener('animationend', handleAnimationEnd, {once: true});
+  });
